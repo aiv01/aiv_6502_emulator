442c1ba5117f340aad50e2830633b08735b3cbdc
Damiano Santarcangelo <damixsant10@gmail.com>
2019-11-26 18:20:52 +0100

    add stx_absolute and test

--- cpu/stx.c
+++ cpu/stx.c
@@ -10,19 +10,43 @@ static int stx_zero_page(mos6502_t *cpu)
 }
 
 
+static int stx_absolute(mos6502_t *cpu)
+{
+    uint16_t address = mos6502_read16(cpu, cpu->pc);
+    cpu->pc+=2;
+    cpu->x = mos6502_read16(cpu, address);
+
+    return 4;
+}
+
+
+
 void mos6502_register_stx(mos6502_t *cpu)
 {
     mos6502_register_opcode(cpu, 0x86, stx_zero_page);
+    mos6502_register_opcode(cpu, 0x8E, stx_absolute);
 }
 
 #ifdef _TEST
 
-static int test_stx_zero(mos6502_t *cpu)
+
+static int test_stx_absolute(mos6502_t *cpu)
 {
-    mos6502_write8(cpu, 0x8000, 0x86);
-   
+    mos6502_write8(cpu, 0x8000, 0x8E);
+    mos6502_write8(cpu, 0x8001, 0x00);
+    mos6502_write8(cpu, 0x8002, 0x44);
+    mos6502_write8(cpu, 0x4400, 0xA);
     int ticks = mos6502_tick(cpu);
-    return ticks == 2 && cpu->x == 0x00 && cpu->pc == 0x8002 && cpu->flags == ZERO;
+
+    return ticks == 4 && cpu->x == 0xA && cpu->pc == 0x8003 && cpu->flags == 0;
+}
+
+
+
+void test_mos6502_stx()
+{
+    RUN_TEST(test_stx_absolute);
+   
 }
 
 #endif

9b65073106c76a634fc87c42cc18571ce685f559
Sambastard71 <39191371+Sambastard71@users.noreply.github.com>
2019-11-26 18:08:01 +0100

    added Eor opcodes

--- cpu/core.c
+++ cpu/core.c
@@ -5,8 +5,7 @@ int mos6502_init(mos6502_t *cpu)
     memset(cpu, 0, sizeof(mos6502_t));
 
     mos6502_register_lda(cpu);
-    mos6502_register_sta(cpu);
-    mos6502_register_and(cpu);
+    mos6502_register_eor(cpu);
 
     cpu->rst = 1;
     cpu->rdy = 1;

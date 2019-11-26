ff6a35c52a29d49e905d8b6414d5cef46db769f6
Mirko Sembiante <mirkosemby@gmail.com>
2019-11-26 17:39:05 +0100

    add TAY

--- cpu/core.c
+++ cpu/core.c
@@ -7,6 +7,7 @@ int mos6502_init(mos6502_t *cpu)
     mos6502_register_lda(cpu);
     mos6502_register_and(cpu);
     mos6502_register_tax(cpu);
+    mos6502_register_tay(cpu);
 
     cpu->rst = 1;
     cpu->rdy = 1;

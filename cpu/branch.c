91f2ece2adab5881a68fcd25c3de2f1592bbd446
marco iafrati <marco.iafrati@gmail.com>
2019-11-28 05:17:09 +0100

    add tests for page boundary

--- cpu/branch.c
+++ cpu/branch.c
@@ -290,10 +290,20 @@ static int test_bpl_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == NEGATIVE;
 }
 
+static int test_bpl_page_boundary(mos6502_t *cpu)
+{
+    mos6502_set_flag(cpu, NEGATIVE, 0x1);
+    mos6502_write8(cpu, 0x8000, 0x10);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == NEGATIVE;
+}
+
 void test_mos6502_bpl()
 {
     RUN_TEST(test_bpl_no_branch);
     RUN_TEST(test_bpl_branch);
+    RUN_TEST(test_bpl_page_boundary);
 }
 
 static int test_bmi_no_branch(mos6502_t *cpu)
@@ -313,10 +323,19 @@ static int test_bmi_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == 0;
 }
 
+static int test_bmi_page_boundary(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x8000, 0x30);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == 0;
+}
+
 void test_mos6502_bmi()
 {
     RUN_TEST(test_bmi_no_branch);
     RUN_TEST(test_bmi_branch);
+    RUN_TEST(test_bmi_page_boundary);
 }
 
 static int test_bvc_no_branch(mos6502_t *cpu)
@@ -336,10 +355,19 @@ static int test_bvc_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == 0;
 }
 
+static int test_bvc_page_boundary(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x8000, 0x50);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == 0;
+}
+
 void test_mos6502_bvc()
 {
     RUN_TEST(test_bvc_no_branch);
     RUN_TEST(test_bvc_branch);
+    RUN_TEST(test_bvc_page_boundary);
 }
 
 static int test_bvs_no_branch(mos6502_t *cpu)
@@ -359,10 +387,20 @@ static int test_bvs_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == OVERFLOW;
 }
 
+static int test_bvs_page_boundary(mos6502_t *cpu)
+{
+    mos6502_set_flag(cpu, OVERFLOW, 0x1);
+    mos6502_write8(cpu, 0x8000, 0x70);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == OVERFLOW;
+}
+
 void test_mos6502_bvs()
 {
     RUN_TEST(test_bvs_no_branch);
     RUN_TEST(test_bvs_branch);
+    RUN_TEST(test_bvs_page_boundary);
 }
 
 static int test_bcc_no_branch(mos6502_t *cpu)
@@ -382,10 +420,19 @@ static int test_bcc_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == 0;
 }
 
+static int test_bcc_page_boundary(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x8000, 0x90);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == 0;
+}
+
 void test_mos6502_bcc()
 {
     RUN_TEST(test_bcc_no_branch);
     RUN_TEST(test_bcc_branch);
+    RUN_TEST(test_bcc_page_boundary);
 }
 
 static int test_bcs_no_branch(mos6502_t *cpu)
@@ -405,10 +452,20 @@ static int test_bcs_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == CARRY;
 }
 
+static int test_bcs_page_boundary(mos6502_t *cpu)
+{
+    mos6502_set_flag(cpu, CARRY, 0x1);
+    mos6502_write8(cpu, 0x8000, 0xB0);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == CARRY;
+}
+
 void test_mos6502_bcs()
 {
     RUN_TEST(test_bcs_no_branch);
     RUN_TEST(test_bcs_branch);
+    RUN_TEST(test_bcs_page_boundary);
 }
 
 static int test_bne_no_branch(mos6502_t *cpu)
@@ -428,10 +485,19 @@ static int test_bne_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == 0;
 }
 
+static int test_bne_page_boundary(mos6502_t *cpu)
+{
+    mos6502_write8(cpu, 0x8000, 0xD0);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == 0;
+}
+
 void test_mos6502_bne()
 {
     RUN_TEST(test_bne_no_branch);
     RUN_TEST(test_bne_branch);
+    RUN_TEST(test_bne_page_boundary);
 }
 
 static int test_beq_no_branch(mos6502_t *cpu)
@@ -451,9 +517,19 @@ static int test_beq_branch(mos6502_t *cpu)
     return ticks == 3 && cpu->pc == 0x8006 && cpu->flags == ZERO;
 }
 
+static int test_beq_page_boundary(mos6502_t *cpu)
+{
+    mos6502_set_flag(cpu, ZERO, 0x1);
+    mos6502_write8(cpu, 0x8000, 0xF0);
+    mos6502_write8(cpu, 0x8001, -5);
+    int ticks = mos6502_tick(cpu);
+    return ticks == 4 && cpu->pc == 0x7FFC && cpu->flags == ZERO;
+}
+
 void test_mos6502_beq()
 {
     RUN_TEST(test_beq_no_branch);
     RUN_TEST(test_beq_branch);
+    RUN_TEST(test_beq_page_boundary);
 }
 #endif

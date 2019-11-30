d6cf9da6eba2c9fdce0ec84ddd5203d6e513259f
LorenzoDiStefano <lorenzodistefano.k@gmail.com>
2019-11-30 16:03:45 +0100

    Fix sta absolute

--- cpu/sta.c
+++ cpu/sta.c
@@ -4,8 +4,7 @@ static int sta_absolute(mos6502_t *cpu)
 {
     uint16_t address = mos6502_read16(cpu, cpu->pc);
     cpu->pc+=2;
-    cpu->a = mos6502_read16(cpu, address);
-
+    mos6502_write8(cpu, address, cpu->a);
     return 4;
 }
 
@@ -18,13 +17,13 @@ void mos6502_register_sta(mos6502_t *cpu)
 
 static int test_sta_absolute(mos6502_t *cpu)
 {
+    cpu->a = 0x49;
     mos6502_write8(cpu, 0x8000, 0x8D);
-    mos6502_write8(cpu, 0x8001, 0x00);
-    mos6502_write8(cpu, 0x8002, 0x44);
+    mos6502_write16(cpu, 0x8001, 0x4400);
     mos6502_write8(cpu, 0x4400, 0xA);
     int ticks = mos6502_tick(cpu);
-
-    return ticks == 4 && cpu->a == 0xA && cpu->pc == 0x8003 && cpu->flags == 0;
+    int new_value = mos6502_read8(cpu,0x4400);
+    return ticks == 4 && cpu->a == 0x49 && cpu->pc == 0x8003 && cpu->flags == 0 && new_value == 0x49;
 }
 
 void test_mos6502_sta()

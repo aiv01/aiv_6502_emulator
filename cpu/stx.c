384fbc100bc1e5f6c8bcb716e24f8c7c442ea96c
Damiano Santarcangelo <damixsant10@gmail.com>
2019-12-03 13:18:55 +0100

    add others stx opcodes

--- cpu/stx.c
+++ cpu/stx.c
@@ -5,8 +5,15 @@ static int stx_zero_page(mos6502_t *cpu)
 {
     uint8_t zp_address = cpu->read(cpu, cpu->pc++);
   
-    mos6502_write8(cpu, (uint16_t)zp_address, (uint8_t) cpu -> x);
+    mos6502_write8(cpu, (uint16_t)zp_address, (uint8_t)cpu->x);
     return 3;
+}
+
+static int stx_zeropage_y(mos6502_t *cpu)
+{
+    uint8_t zp_address = (mos6502_read8(cpu, cpu->pc++)) + cpu->y;
+    mos6502_write8(cpu, (uint16_t)zp_address, cpu->x);
+    return 4;
 }
 
 
@@ -24,11 +31,33 @@ static int stx_absolute(mos6502_t *cpu)
 void mos6502_register_stx(mos6502_t *cpu)
 {
     mos6502_register_opcode(cpu, 0x86, stx_zero_page);
+    mos6502_register_opcode(cpu, 0x86, stx_zeropage_y);
     mos6502_register_opcode(cpu, 0x8E, stx_absolute);
 }
 
 #ifdef _TEST
 
+static int test_stx_zeropage(mos6502_t *cpu)
+{
+    cpu->x=0x49;
+    mos6502_write8(cpu, 0x8000, 0x8E);
+    mos6502_write8(cpu, 0x8001, 0x44);
+    mos6502_write8(cpu, 0x0044, 0xA);
+    int ticks = mos6502_tick(cpu);
+    int new_value = mos6502_read8(cpu,0x44);
+    return ticks == 3 && cpu->x == 0x49 && cpu->pc == 0x8002 && cpu->flags == 0 && new_value == 0x49;
+}
+
+static int test_stx_zeropage_y(mos6502_t *cpu)
+{
+    cpu->x = 0x49;
+    cpu->y = 0x10;    
+    mos6502_write8(cpu, 0x8000, 0x8E);
+    mos6502_write8(cpu, 0x8001, 0x40);
+    int ticks = mos6502_tick(cpu);
+    uint8_t new_value = mos6502_read8(cpu,0x0050);
+    return ticks == 4 && cpu->x == 0x49 && cpu->pc == 0x8002 && cpu->flags == 0 && new_value == 0x49;
+}
 
 static int test_stx_absolute(mos6502_t *cpu)
 {
@@ -43,8 +72,13 @@ static int test_stx_absolute(mos6502_t *cpu)
 
 
 
+
+
 void test_mos6502_stx()
 {
+    
+    RUN_TEST(test_stx_zeropage);
+    RUN_TEST(test_stx_zeropage_y);
     RUN_TEST(test_stx_absolute);
    
 }

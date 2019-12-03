1a9f92b274528d96b409ed8cbe9d590e7d0b5782
Tommaso Allegri <allegritommaso@hotmail.it>
2019-12-03 16:07:14 +0000

    Fixed CMP negative flag implementation
    Fixed CMP negative flag implementation

--- cpu/cmp.c
+++ cpu/cmp.c
@@ -4,43 +4,39 @@ static int cmp_immediate(mos6502_t *cpu){
     uint8_t immediate = cpu->read(cpu, cpu->pc++);   
 
     int z_flag_value = cpu->a == immediate;
-    int c_flag_value = cpu->a >= immediate;
-    
-    //int sub_value = cpu->a - immediate;
+    int c_flag_value = cpu->a >= immediate;    
+    int sub_value = cpu->a - immediate;
     //The N flag contains most significant bit 
     //of the of the subtraction result.
-    
-    //fprintf(stdout, "immediate: %hhu\na: %hhu\nz_flag: %d\nc_flag: %d\n", immediate, cpu->a, z_flag_value, c_flag_value);  
+    mos6502_set_flag(cpu, NEGATIVE, sub_value < 0);
+
     mos6502_set_flag(cpu, CARRY, c_flag_value);
     //if CARRY == 0 then A < value
     //  ->  can BCC
     //else if ZERO == 1 then A >= value
     //  ->  can BCS
+
     mos6502_set_flag(cpu, ZERO, z_flag_value);
     //if ZERO == 0 then A != value
-    if(mos6502_get_flag(cpu, ZERO) == 0){
-        return 2;   // -> can BNE
-    }//else if ZERO == 1 then A == value
-    else if(mos6502_get_flag(cpu, ZERO) == 1){
-        return 3;   // -> can BEQ
-    }
-    return 0;
+    // -> can BNE
+    //else if ZERO == 1 then A == value
+    // -> can BEQ
+
+    return 2;
 }
 static int cmp_zero_page(mos6502_t *cpu){
     uint8_t zp_address = cpu->read(cpu, cpu->pc++);   
     uint8_t zp_value = mos6502_read8(cpu, (uint16_t)zp_address);
+
     int z_flag_value = cpu->a == zp_value;
     int c_flag_value = cpu->a >= zp_value;
-    
+    int sub_value = cpu->a - zp_value;
+
+    mos6502_set_flag(cpu, NEGATIVE, sub_value < 0);
     mos6502_set_flag(cpu, CARRY, c_flag_value);
     mos6502_set_flag(cpu, ZERO, z_flag_value);
-    if(mos6502_get_flag(cpu, ZERO) == 0){
-        return 2;
-    }
-    else if(mos6502_get_flag(cpu, ZERO) == 1){
-        return 3;
-    }
-    return 0;
+    
+    return 3;
 }
 static int cmp_absolute(mos6502_t *cpu){
     uint8_t low = cpu->read(cpu, cpu->pc++);
@@ -50,16 +46,13 @@ static int cmp_absolute(mos6502_t *cpu){
 
     int z_flag_value = cpu->a == value;
     int c_flag_value = cpu->a >= value;
+    int sub_value = cpu->a - value;
 
+    mos6502_set_flag(cpu, NEGATIVE, sub_value < 0);
     mos6502_set_flag(cpu, CARRY, c_flag_value);
     mos6502_set_flag(cpu, ZERO, z_flag_value);
-    if(mos6502_get_flag(cpu, ZERO) == 0){
-        return 2;
-    }
-    else if(mos6502_get_flag(cpu, ZERO) == 1){
-        return 3;
-    }
-    return 0;
+
+    return 4;
 }
 static int cmp_absolute_x(mos6502_t *cpu){
     uint8_t low = cpu->read(cpu, cpu->pc++);
@@ -70,16 +63,13 @@ static int cmp_absolute_x(mos6502_t *cpu){
 
     int z_flag_value = cpu->a == value;
     int c_flag_value = cpu->a >= value;
+    int sub_value = cpu->a - value;
 
+    mos6502_set_flag(cpu, NEGATIVE, sub_value < 0);
     mos6502_set_flag(cpu, CARRY, c_flag_value);
     mos6502_set_flag(cpu, ZERO, z_flag_value);
-    if(mos6502_get_flag(cpu, ZERO) == 0){
-        return 2;
-    }
-    else if(mos6502_get_flag(cpu, ZERO) == 1){
-        return 3;
-    }
-    return 0;
+
+    return 4;
 }
 static int cmp_absolute_y(mos6502_t *cpu){
     uint8_t low = cpu->read(cpu, cpu->pc++);
@@ -90,15 +80,21 @@ static int cmp_absolute_y(mos6502_t *cpu){
 
     int z_flag_value = cpu->a == value;
     int c_flag_value = cpu->a >= value;
+    int sub_value = cpu->a - value;
 
+    mos6502_set_flag(cpu, NEGATIVE, sub_value < 0);
     mos6502_set_flag(cpu, CARRY, c_flag_value);
     mos6502_set_flag(cpu, ZERO, z_flag_value);
-    if(mos6502_get_flag(cpu, ZERO) == 0){
-        return 2;
-    }
-    else if(mos6502_get_flag(cpu, ZERO) == 1){
-        return 3;
-    }
+
+    return 4;
+}
+
+//CMP ($F6,X)	== A - contents of address at $F6 + X
+static int cmp_indirect_x(mos6502_t *cpu){   
+    return 0;
+}
+//CMP ($F6),Y   == A - contents of (address at $F6) + offset Y
+static int cmp_indirect_y(mos6502_t *cpu){
     return 0;
 }
 
@@ -110,6 +106,9 @@ void mos6502_register_cmp(mos6502_t *cpu){
     mos6502_register_opcode(cpu, 0xCD, cmp_absolute);
     mos6502_register_opcode(cpu, 0xDD, cmp_absolute_x);
     mos6502_register_opcode(cpu, 0xD9, cmp_absolute_y);
+
+    mos6502_register_opcode(cpu, 0xC1, cmp_indirect_x);
+    mos6502_register_opcode(cpu, 0xD1, cmp_indirect_x);
 }
 
 #ifdef _TEST
@@ -121,7 +120,8 @@ static int test_cmp_a_greater_immediate(mos6502_t *cpu){
     int ticks = mos6502_tick(cpu);
     int zero_flag = mos6502_get_flag(cpu, ZERO);
     int carry_flag = mos6502_get_flag(cpu, CARRY);
-    return zero_flag == 0 && carry_flag == 1;
+    int negative_flag = mos6502_get_flag(cpu, NEGATIVE);
+    return zero_flag == 0 && carry_flag == 1 && negative_flag == 0;
 }
 static int test_cmp_a_less_immediate(mos6502_t *cpu){
     cpu->a = 3;
@@ -130,7 +130,8 @@ static int test_cmp_a_less_immediate(mos6502_t *cpu){
     int ticks = mos6502_tick(cpu);
     int zero_flag = mos6502_get_flag(cpu, ZERO);
     int carry_flag = mos6502_get_flag(cpu, CARRY);
-    return zero_flag == 0 && carry_flag == 0;
+    int negative_flag = mos6502_get_flag(cpu, NEGATIVE);
+    return zero_flag == 0 && carry_flag == 0 && negative_flag == 1;
 }
 static int test_cmp_a_equal_immediate(mos6502_t *cpu){
     cpu->a = 8;
@@ -139,7 +140,8 @@ static int test_cmp_a_equal_immediate(mos6502_t *cpu){
     int ticks = mos6502_tick(cpu);
     int zero_flag = mos6502_get_flag(cpu, ZERO);
     int carry_flag = mos6502_get_flag(cpu, CARRY);
-    return zero_flag == 1 && carry_flag == 1;
+    int negative_flag = mos6502_get_flag(cpu, NEGATIVE);
+    return zero_flag == 1 && carry_flag == 1 && negative_flag == 0;
 }
 
 static int test_cmp_greater_absolute(mos6502_t *cpu){
